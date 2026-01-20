311a0819f4fd10b36831121a596829077df68614
deadsxnpai <deadsxnpai@gmail.com>
2026-01-20 09:25:21 +0300

    fix: useColortheme fix

--- src/shared/lib/hooks/use-theme-color.ts
+++ src/shared/lib/hooks/use-theme-color.ts
@@ -4,13 +4,12 @@
  */
 
 import { Colors } from '@/shared/constants/theme';
-import { useColorScheme } from '@/shared/lib/hooks/use-color-scheme';
 
 export function useThemeColor(
 	props: { light?: string; dark?: string },
-	colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+	colorName: keyof typeof Colors.light,
 ) {
-	const theme = useColorScheme() ?? 'light';
+	const theme = 'light';
 	const colorFromProps = props[theme];
 
 	if (colorFromProps) {
