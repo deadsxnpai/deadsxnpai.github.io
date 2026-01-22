36cd4e9f08c8f6ce0aea7eff43e51c2512093d62
deadsxnpai <deadsxnpai@gmail.com>
2026-01-22 09:48:05 +0300

    fix: not fount page fix

--- src/app/+not-found.tsx
+++ src/app/+not-found.tsx
@@ -1,23 +1,40 @@
 // app/+not-found.tsx
-import { Colors } from '@/shared/config/theme';
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Container, Typography } from '@/shared';
+import { Header } from '@/widgets';
+import { router } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
 
 export default function NotFoundScreen() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Страница не найдена</Text>
-			<Link
-				href='/'
-				style={styles.link}>
-				Назад
-			</Link>
-		</View>
+		<Container>
+			<Header title='' />
+			<View style={styles.section}>
+				<Typography
+					variant='h1'
+					style={styles.title}>
+					Страница не найдена
+				</Typography>
+				<Button
+					title='Вернуться на главную'
+					onPress={() => router.replace('/')}
+					variant='primary'
+				/>
+			</View>
+		</Container>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
-	title: { fontSize: 20, fontWeight: 'bold' },
-	link: { marginTop: 16, color: Colors.primary },
+	content: {
+		flex: 1,
+		paddingHorizontal: 16,
+	},
+	section: {
+		marginTop: '60%',
+
+		padding: 16,
+		borderRadius: 12,
+		elevation: 3,
+	},
+	title: { textAlign: 'center', width: '100%', marginBottom: 50 },
 });
