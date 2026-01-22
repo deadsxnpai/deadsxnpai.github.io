08eb0e1dfa484a718c4e5f7f59ec57a913a955bf
deadsxnpai <deadsxnpai@gmail.com>
2026-01-22 09:56:06 +0300

    fix: hotfix

--- src/pages/chatbot/chatbot-screen.tsx
+++ src/pages/chatbot/chatbot-screen.tsx
@@ -2,19 +2,5 @@ import { CrossPlatformWebView } from '@/shared';
 import { EndPoints } from '@/shared/config/base';
 
 export function ChatbotScreen() {
-	const injectedJs = `
-    function jivo_onLoadCallback() {
-      jivo_api.setContactInfo({
-        name: "$Поумолчанов Новый Пользович",
-        email: "kluchnikovds@tsutmb.ru",
-        phone: "+79005125355"
-      });
-    }
-  `;
-	return (
-		<CrossPlatformWebView
-			url={EndPoints.chatbotStudent}
-			injectedJavaScript={injectedJs}
-		/>
-	);
+	return <CrossPlatformWebView url={EndPoints.chatbotStudent} />;
 }
