a494e49c2e826d9d980c2b97fbd2b66bcd645963
deadsxnpai <deadsxnpai@gmail.com>
2026-04-09 15:44:36 +0300

    fix: hotfix

--- src/features/profile/ui/student-info-view.tsx
+++ src/features/profile/ui/student-info-view.tsx
@@ -1,7 +1,6 @@
 import { useStudentInfo } from '@/features/profile/api/hooks/use-student-info';
 import { InfoViewStyles } from '@/features/profile/model/types';
 import { InfoView, Loader } from '@/shared/ui';
-import { ErrorView } from '@/widgets/error-view';
 import { Text, View } from 'react-native';
 
 export const StudentInfoView = ({
@@ -14,11 +13,7 @@ export const StudentInfoView = ({
 		return <Loader />;
 	}
 
-	if (error) {
-		return <ErrorView error={error} />;
-	}
-
-	if (!data) {
+	if (!data || error) {
 		return (
 			<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
 				<Text>Нет данных о студенте</Text>
