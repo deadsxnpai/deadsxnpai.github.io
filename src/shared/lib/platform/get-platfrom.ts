1170e6d1fad1d5d3b2ccc5b436aaee72a8533b95
deadsxnpai <deadsxnpai@gmail.com>
2026-06-24 14:18:45 +0300

    fix: hotfix

--- src/shared/lib/platform/get-platfrom.ts
+++ src/shared/lib/platform/get-platfrom.ts
@@ -27,7 +27,6 @@ export const detectPlatform = (): AppPlatform => {
         const RNPlatform = require('react-native').Platform;
         if (RNPlatform.OS === 'ios') return 'ios';
         if (RNPlatform.OS === 'android') return 'android';
-        if (RNPlatform.OS === 'web') return 'web';
     } catch {
     }
     if (typeof window !== 'undefined') {
